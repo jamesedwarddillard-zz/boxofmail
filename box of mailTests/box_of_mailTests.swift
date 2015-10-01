b4f1b8c3e8ad298ab21ebc74acda764af46148d7
James Dillard <jamesedwarddillard@gmail.com>
2015-09-30 17:32:29 -0700

    Initial Commit

---
+++ box of mailTests/box_of_mailTests.swift
@@ -0,0 +1,36 @@
+//
+//  box_of_mailTests.swift
+//  box of mailTests
+//
+//  Created by James Dillard on 9/30/15.
+//  Copyright © 2015 James Dillard. All rights reserved.
+//
+
+import XCTest
+@testable import box_of_mail
+
+class box_of_mailTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
